f729bc64715d31c7255b1e065f684ac958981634
mleilani <mlfelipe@cpp.edu>
2022-12-16 16:07:43 -0800

    update to sub

--- src/subscriber.c
+++ src/subscriber.c
@@ -14,4 +14,23 @@ int main() {
     struct mosquitto *mosq;
 
     mosq = mosquitto_new("subscribe-test", true, &id);
+
+   rc = mosquitto_connect(mosq, "test.mosquitto.org", 1883, 10);
+
+   if (rc) {
+    printf("Unable to connect to broker woth %d\n", rc);
+    return -1;
+   }
+
+   mosquitto_loop_start(mosq);
+   printf("Press Enter to quit\n");
+   getchar();
+   mosquitto_loop_end(mosq, true);
+
+   mosquitto_disconnect(mosq);
+   mosquitto_destroy(mosq);
+
+   mosquitto_lib_cleanup();
+
+   return EXIT_SUCCESS;
 }
